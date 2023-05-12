e2e3db0536eea24110b4f6f1a6bdf3007a15325c
Rudhran.B <90748029+KAVIN75@users.noreply.github.com>
2023-05-12 22:48:22 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello
 {
   public static void main(String args[])
   {
-    System.out.print("Hello World!");
+    System.out.println("Hello World!");
     System.out.println("Hello World!");
   }
 }
